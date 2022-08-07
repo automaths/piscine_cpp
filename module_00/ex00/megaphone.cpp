e7da5cc542b5b8144dc49a006d395feca27dca69
nimrodds <nsartral@student.42.fr>
2022-08-07 19:08:06 +0200

    megaphone finished

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,10 +6,38 @@ using namespace std;
 
 class	Speech{
 	private:
-	string speech;
+	string sentence;
 	public:
-	Speech(string str){speech = str;}
-	void addElement(string str){speech.append(str);}
-	void shoutOut(){std::cout << speech << std::endl;}
-}
+	Speech(){sentence = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";}
+	Speech(string str){sentence = str;}
+	void addElement(string str)
+	{
+		str.erase(0, str.find_first_not_of(" "));
+		str.erase(str.find_last_not_of(" ") + 1, str.size() - str.find_last_not_of(" "));
+		sentence.append(" ");
+		sentence.append(str);
+	}
+	void shoutOut()
+	{
+		for (int i = 0; i != sentence.size(); ++i)
+			cout << (char)toupper(sentence[i]);
+		cout << "" << endl;
+	}
+};
 
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		Speech megaphone;
+		megaphone.shoutOut();
+	}
+	else
+	{
+		int i = 1;
+		Speech megaphone(argv[1]);
+		while (++i < argc)
+			megaphone.addElement(argv[i]);
+		megaphone.shoutOut();
+	}
+}
