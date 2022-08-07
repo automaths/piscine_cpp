b49f6c741246324e68e8561380fe8f3782751dd3
nimrodds <nsartral@student.42.fr>
2022-08-07 19:15:03 +0200

    megaphone corrected

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -10,13 +10,7 @@ class	Speech{
 	public:
 	Speech(){sentence = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";}
 	Speech(string str){sentence = str;}
-	void addElement(string str)
-	{
-		str.erase(0, str.find_first_not_of(" "));
-		str.erase(str.find_last_not_of(" ") + 1, str.size() - str.find_last_not_of(" "));
-		sentence.append(" ");
-		sentence.append(str);
-	}
+	void addElement(string str){sentence.append(str);}
 	void shoutOut()
 	{
 		for (int i = 0; i != sentence.size(); ++i)
